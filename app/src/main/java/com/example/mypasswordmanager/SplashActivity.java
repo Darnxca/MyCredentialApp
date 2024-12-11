96c8ed7a2a1c0385d005d212473765ad562f788a
darnxca <c.dangelo23@studenti.unisa.it>
2024-12-11 11:36:28 +0100

    Aggiunta autenticazione biometrica

--- app/src/main/java/com/example/mypasswordmanager/SplashActivity.java
+++ app/src/main/java/com/example/mypasswordmanager/SplashActivity.java
@@ -3,17 +3,25 @@ package com.example.mypasswordmanager;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.splashscreen.SplashScreen;
 
+import java.util.concurrent.Executor;
 
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         applySavedTheme();
@@ -21,9 +29,26 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         splashScreen.setKeepOnScreenCondition(() -> true );
-        Intent myIntent = new Intent(SplashActivity.this, MainActivity.class);
-        SplashActivity.this.startActivity(myIntent);
-        finish();
+
+        // Verifica se la biometria è disponibile sul dispositivo
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(
+                BiometricManager.Authenticators.BIOMETRIC_STRONG |
+                        BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                // La biometria è disponibile
+                authenticateUser();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "Il dispositivo non supporta la biometria", Toast.LENGTH_LONG).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "La biometria non è disponibile", Toast.LENGTH_LONG).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this, "Nessuna credenziale biometrica configurata", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
     private void applySavedTheme() {
@@ -36,4 +61,50 @@ public class SplashActivity extends AppCompatActivity {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void authenticateUser() {
+        // Esegui l'autenticazione biometrica o di sistema
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt biometricPrompt = new BiometricPrompt(this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                goToMainActivity();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                // Autenticazione fallita
+                Toast.makeText(SplashActivity.this, "Autenticazione fallita", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                // Errore di autenticazione
+                Toast.makeText(SplashActivity.this, "Errore: " + errString, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Configura il prompt di autenticazione
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Autenticazione richiesta")
+                .setSubtitle("Utilizza l'autenticazione biometrica o le credenziali del dispositivo")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG |
+                        BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                .build();
+
+        // Mostra il prompt di autenticazione
+        biometricPrompt.authenticate(promptInfo);
+    }
+
+    private void goToMainActivity() {
+        Intent myIntent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(myIntent);
+        finish(); // Chiudi l'activity corrente
+    }
+
+
 }
